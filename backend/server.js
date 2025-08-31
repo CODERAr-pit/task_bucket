b0650c891aaa9afbf9d3bfbd2f90fa4c5444777d
Subhadip006 <subhadip58512@gmail.com>
2025-08-31 12:30:12 +0530

    change url for cors resolve

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ import './services/cronJobs.js';
 // Middleware
 // CORS configuration
 app.use(cors({
-    origin: [process.env.CLIENT_URL || 'http://localhost:3000', process.env.CLIENT_URL || 'https://task-bucket.onrender.com'],
+    origin: [process.env.CLIENT_URL || 'http://localhost:3000', process.env.CLIENT_URL || 'https://task-bucket.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
